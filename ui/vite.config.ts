e2e6956497aef01ef9c51ec37128e1a27228ed66
Allie Howe <32961755+howe-oh@users.noreply.github.com>
2025-08-20 16:17:50 +0100

    gif-canvas lessons: adding tailwind in vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './', // changes base URL from '/' to relative - needed for subpath hosting
+  build: {
+    outDir: 'fullstack-site'
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     proxy: {
       // Proxy API requests to the backend server
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        // No rewrite needed, keep the /api prefix
       }
     }
   }
